27955c377101a63f722d40eacb8f57b450897c54
noahthomlison <noahthomlison@gmail.com>
2021-10-10 21:49:01 +0000

    adds lightbnb database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,7 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg')
 
 /// Users
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
 
 /**
  * Get a single user from the database given their email.

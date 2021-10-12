b21b43387bcf866cc9f69e88692e7f3527cdd2ce
noahthomlison <noahthomlison@gmail.com>
2021-10-12 04:33:20 +0000

    starts implimentation of filters for getting reservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -121,24 +121,54 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  pool
+  const queryParams = [];
+  let queryString = 
+  "SELECT properties.*, AVG(property_reviews.rating) AS average_rating\
+  FROM properties\
+  JOIN property_reviews ON properties.id = property_reviews.property_id"
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString +=  `WHERE owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.maximum_price_per_night}`);
+
+    queryString += ` WHERE properties.cost_per_night > $${queryParams.length-1} AND properties.cost_per_night < $${queryParams.length}`;
+  }
+
+  queryString += ' GROUP BY properties.id'
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool
     .query(
-      `SELECT * FROM properties
-      LIMIT $1`,
-      [ limit ])
+      queryString,
+      queryParams)
     .then((result) => {
-      // console.log(result.rows);
       return result.rows;
       })
     .catch((err) => {
       console.log(err.message);
       });
-      
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
 }
 exports.getAllProperties = getAllProperties;
 
